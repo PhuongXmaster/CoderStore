eeef574b766621128e5ea42bc9727afbacfdf27f
Phuong Doan <pdoan@atlassian.com>
2015-12-17 19:25:00 +0700

    milestone 1

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151125095428) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
